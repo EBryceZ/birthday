afedb8366a21dc52462d1e1ad2d89ef5b648a4bb
Bryce Zhong <36309961+EBryceZ@users.noreply.github.com>
2022-03-08 12:27:21 -0500

    Update effect.js

--- assets/effect.js
+++ assets/effect.js
@@ -150,15 +150,15 @@ $('document').ready(function(){
 		$('#b5').attr('id','b55')
 		$('#b6').attr('id','b66')
 		$('#b7').attr('id','b77')
-		$('#b11').animate({top:150, left: vw-350},500);
-		$('#b22').animate({top:150, left: vw-250},500);
-		$('#b33').animate({top:100, left: vw-150},500);
-		$('#b44').animate({top:100, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
-		$('.balloons').css('opacity','0.9');
-		$('.balloons h2').fadeIn(3000);
+		//$('#b11').animate({top:150, left: vw-350},500);
+		//$('#b22').animate({top:150, left: vw-250},500);
+		//$('#b33').animate({top:100, left: vw-150},500);
+		//$('#b44').animate({top:100, left: vw-50},500);
+		//$('#b55').animate({top:240, left: vw+50},500);
+		//$('#b66').animate({top:240, left: vw+150},500);
+		//$('#b77').animate({top:240, left: vw+250},500);
+		//$('.balloons').css('opacity','0.9');
+		//$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(4000).promise().done(function(){
 			$('#story').fadeIn('slow');
 		});
